02d0cbf099a075fd781cadbeb8e8d6cf6771d4d6
andrsweb <d3vv.proo@gmail.com>
2024-07-20 16:16:41 +0700

    Created dual programs

--- wp-content/themes/livanta/page-templates/dual361.php
+++ wp-content/themes/livanta/page-templates/dual361.php
@@ -8,6 +8,8 @@
  */
 
 get_header();
+wp_enqueue_style( 'dual_programs', THEME_URI . '/static/css/dual_programs/dual_programs.min.css', [], THEME_VERSION );
+// wp_enqueue_script( 'blog_articles', THEME_URI . '/static/js/blog_articles/blog_articles.min.js', ['jquery'], THEME_VERSION, true ); Не удаляй 2 строки, для примера будут.
 ?>
 
 <main class="main">
@@ -37,6 +39,75 @@ get_header();
 		}
 	}
 	?>
+	<section class="dual-programs">
+		<div class="container">
+			<div class="dual-programs-wrapper">
+				<h2>
+					Emphasizing preventive care and early intervention, our programs and materials include: 
+				</h2>
+				<div class="dual-programs-items">
+					<div class="dual-programs-item">
+						<div class="dual-programs-item-img">
+							<img src="<?php echo THEME_URI ?>/static/img/dual361/dp1.svg" alt="">
+						</div>
+						<p>
+							Resources and guidance to help employees adopt healthy lifestyles
+						</p>
+					</div>
+					<div class="dual-programs-item">
+						<div class="dual-programs-item-img">
+							<img src="<?php echo THEME_URI ?>/static/img/dual361/dp2.svg" alt="">
+						</div>
+						<p>
+							Regular health screenings
+						</p>
+					</div>
+					<div class="dual-programs-item">
+						<div class="dual-programs-item-img">
+							<img src="<?php echo THEME_URI ?>/static/img/dual361/dp3.svg" alt="">
+						</div>
+						<p>
+							Wellness programs
+						</p>
+					</div>
+					<div class="dual-programs-item">
+						<div class="dual-programs-item-img">
+							<img src="<?php echo THEME_URI ?>/static/img/dual361/dp4.svg" alt="">
+						</div>
+						<p>
+							Health education materials
+						</p>
+					</div>
+					<div class="dual-programs-item">
+						<div class="dual-programs-item-img">
+							<img src="<?php echo THEME_URI ?>/static/img/dual361/dp5.svg" alt="">
+						</div>
+						<p>
+							Support for employees who are returning to work after an illness or injury 
+						</p>
+					</div>
+				</div>
+				<a href="#" class="link-arrow">
+					<span>
+						Learn more about our Absenteeism Reduction program
+						<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<mask
+								id="1" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="24"
+							>
+								<rect x="0.5" width="24" height="24" fill="#D9D9D9"/>
+							</mask>
+							<g mask="url(#1">
+								<path
+									d="M17.1269 12.75H5V11.25H17.1269L11.4308 5.55383L12.5 4.5L20 12L12.5 19.5L11.4308 18.4461L17.1269 12.75Z"
+									fill="#BF1E2E"
+								/>
+							</g>
+						</svg>
+					</span>
+				</a>
+			</div>
+		</div>
+	</section>
 </main>
 <?php
 get_footer();
