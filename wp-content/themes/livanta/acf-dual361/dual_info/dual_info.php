8ba3d2d2b13404ea62757d7898dd9e49171be706
Andrei Stezenko <astezenko@markupus.tech>
2024-07-22 23:52:53 +0300

    upd: add link to Info image.

--- wp-content/themes/livanta/acf-dual361/dual_info/dual_info.php
+++ wp-content/themes/livanta/acf-dual361/dual_info/dual_info.php
@@ -15,6 +15,7 @@ $title      = get_sub_field( 'title' );
 $text_title = get_sub_field( 'text_title' );
 $text       = get_sub_field( 'text' );
 $image      = get_sub_field( 'image' );
+$image_link = get_sub_field( 'image_link' );
 ?>
 
 <section class="dual-info">
@@ -27,13 +28,29 @@ $image      = get_sub_field( 'image' );
 				echo '<div class="dual-info-text">';
 
 				if( $image ){
-					?>
-					<div class="dual-text-img-wrapper">
-						<div class="dual-text-img">
-							<?php echo wp_get_attachment_image( $image, 'medium' ) ?>
-						</div>
-					</div>
-					<?php
+					echo '<div class="dual-text-img-wrapper">';
+
+					if( $image_link ){
+						$image_link_title  = $image_link['title'] ?? '';
+						$image_link_url    = $image_link['url'] ?? '#';
+						$image_link_target = $image_link['target'] ? ' target="_blank"' : '';
+						?>
+						<a
+							class="dual-text-img"
+							href="<?php echo esc_url( $image_link_url ) ?>"
+							title="<?php echo esc_attr( $image_link_title ) ?>"
+							<?php echo $image_link_target ?>
+						>
+						<?php
+					}else {
+						echo '<div class="dual-text-img">';
+					}
+
+					echo wp_get_attachment_image( $image, 'medium' );
+
+					echo $image_link ? '</a>' : '</div>';
+
+					echo '</div>';
 				}
 
 				if( $text_title ) echo '<div class="dual-text-title">', $text_title, '</div>';
