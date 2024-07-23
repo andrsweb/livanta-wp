2880fdc36020fca8981a676f2364eca50976715a
Andrei Stezenko <astezenko@markupus.tech>
2024-07-23 14:47:44 +0300

    fix: footer.

--- wp-content/themes/livanta/footer.php
+++ wp-content/themes/livanta/footer.php
@@ -2,7 +2,7 @@
 
 if( is_page() && is_page_template( 'page-templates/dual361.php' ) )
 	get_template_part( 'template-parts/footers/dual361' );  // DUAL361 page template footer.
-if( is_page() && is_page_template( 'page-templates/login-member361.php' ) )
+else if( is_page() && is_page_template( 'page-templates/login-member361.php' ) )
 	get_template_part( 'template-parts/footers/login-member361' );  // Login MEMBER361 page template footer.
 else if( is_page() && is_page_template( 'page-templates/login-empower361.php' ) )
 	get_template_part( 'template-parts/footers/login-empower361' ); // Login EMPOWER361 page template footer.
