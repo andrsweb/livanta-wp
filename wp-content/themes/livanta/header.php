0aca630d601e6409df0c9ad2b8c20d4b7f74e5fc
andrsweb <d3vv.proo@gmail.com>
2024-08-01 05:49:39 +0700

    Added checks for footer

--- wp-content/themes/livanta/header.php
+++ wp-content/themes/livanta/header.php
@@ -1,10 +1,11 @@
 <?php
 
-if( is_page() && is_page_template( 'page-templates/dual361.php' ) )
-	get_template_part( 'template-parts/headers/dual361' );  // DUAL361 page template header.
-else if( is_page() && is_page_template( 'page-templates/login-member361.php' ) )
-	get_template_part( 'template-parts/headers/login-member361' );  // Login MEMBER361 page template header.
-else if( is_page() && is_page_template( 'page-templates/login-empower361.php' ) )
-	get_template_part( 'template-parts/headers/login-empower361' ); // Login EMPOWER361 page template header.
-else get_template_part( 'template-parts/headers/default' ); // Default header.
-
+if ( is_page() && ( is_page_template( 'page-templates/dual361.php' ) || is_page_template( 'page-templates/dual361-blog.php' ) ) ) {
+    get_template_part( 'template-parts/headers/dual361' );  // DUAL361 page template header.
+} elseif ( is_page() && is_page_template( 'page-templates/login-member361.php' ) ) {
+    get_template_part( 'template-parts/headers/login-member361' );  // Login MEMBER361 page template header.
+} elseif ( is_page() && is_page_template( 'page-templates/login-empower361.php' ) ) {
+    get_template_part( 'template-parts/headers/login-empower361' ); // Login EMPOWER361 page template header.
+} else {
+    get_template_part( 'template-parts/headers/default' ); // Default header.
+}
